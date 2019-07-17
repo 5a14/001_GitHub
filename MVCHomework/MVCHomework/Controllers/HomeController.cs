6fac5605caf1ae127ea938c9d59c750f3b12cb13
5a14 <52806714+5a14@users.noreply.github.com>
2019-07-17 15:38:20 +0800

    20190717加入DB利用Entities方式顯示資料
    20190717
    加入DB
    利用Entities連資料庫
    Controller直接連Entities顯示於View

--- MVCHomework/MVCHomework/Controllers/HomeController.cs
+++ MVCHomework/MVCHomework/Controllers/HomeController.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCHomework.Models;
 
 namespace MVCHomework.Controllers
 {
     public class HomeController : Controller
     {
+        SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -19,22 +22,40 @@ namespace MVCHomework.Controllers
         {
             var ListBookkeeping = new List<BookkeepingViewModels>();
 
-            //亂數種子
-            Random rdm = new Random();
+            #region
 
-            for (int i = 0; i < 100; i++)
+            ////亂數種子
+            //Random rdm = new Random();
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    var Bookkeeping = new BookkeepingViewModels();
+
+            //    ////類別與金額的隨機資料
+            //    //int intRdm = rdm.Next(1, 1500);
+
+            //    ////類別資料
+            //    //Bookkeeping.category = ((intRdm % 2 == 0) ? "支出" : "收入");
+            //    ////金額
+            //    //Bookkeeping.money = intRdm;
+            //    ////日期
+            //    //Bookkeeping.date = DateTime.Now.AddDays(-i);
+
+            //    ////資料加入List中
+            //    //ListBookkeeping.Add(Bookkeeping);
+            //}
+            #endregion
+
+            foreach (var Item in db.AccountBook)
             {
                 var Bookkeeping = new BookkeepingViewModels();
 
-                //類別與金額的隨機資料
-                int intRdm = rdm.Next(1, 1500);
-
                 //類別資料
-                Bookkeeping.category = ((intRdm % 2 == 0) ? "支出" : "收入");
+                Bookkeeping.category = Item.Categoryyy == 0? "支出":"收入";
                 //金額
-                Bookkeeping.money = intRdm;
+                Bookkeeping.money = Item.Amounttt;
                 //日期
-                Bookkeeping.date = DateTime.Now.AddDays(-i);
+                Bookkeeping.date = Item.Dateee;
 
                 //資料加入List中
                 ListBookkeeping.Add(Bookkeeping);
